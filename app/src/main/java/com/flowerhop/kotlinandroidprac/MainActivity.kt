2a168886e9780b200ec29ee64bb7af518eafd5b0
Flowerhop <l941166@hotmail.com>
2021-06-18 22:40:27 +0800

    Play viewType.

--- app/src/main/java/com/flowerhop/kotlinandroidprac/MainActivity.kt
+++ app/src/main/java/com/flowerhop/kotlinandroidprac/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatCheckBox
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,48 +20,80 @@ class MainActivity : AppCompatActivity() {
         todoList.adapter = adapter
         todoList.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
         todoList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        adapter.refresh(listOf<TodoItem>(
-            TodoItem("todoItem 1", true),
-            TodoItem("todoItem 2", true),
-            TodoItem("todoItem 3", true),
-            TodoItem("todoItem 4", true),
-            TodoItem("todoItem 5", true),
-            TodoItem("todoItem 6", true),
-            TodoItem("todoItem 7", true),
-            TodoItem("todoItem 8", true),
+        adapter.refresh(listOf<Todo>(
+            Todo.Title("Title 1"),
+            Todo.Item("todoItem 1", true),
+            Todo.Item("todoItem 2", true),
+            Todo.Item("todoItem 3", true),
+            Todo.Item("todoItem 4", true),
+            Todo.Title("Title 2"),
+            Todo.Item("todoItem 5", true),
+            Todo.Item("todoItem 6", true),
+            Todo.Item("todoItem 7", true),
+            Todo.Item("todoItem 8", true),
+            Todo.Item("todoItem 8", true),
+            Todo.Item("todoItem 9", true),
+            Todo.Item("todoItem 10", true),
+            Todo.Item("todoItem 11", true),
+            Todo.Item("todoItem 12", true),
         ))
     }
 }
 
-class TodoListAdapter: RecyclerView.Adapter<TodoItemViewHolder>() {
-    var todoItems = listOf<TodoItem>()
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoItemViewHolder {
-        return TodoItemViewHolder(parent)
+class TodoListAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    var todos = listOf<Todo>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            Todo.VIEW_TYPE_TITLE -> TodoTitleViewHolder(parent)
+            else -> TodoItemViewHolder(parent)
+        }
     }
 
-    override fun onBindViewHolder(holder: TodoItemViewHolder, position: Int) {
-        holder.bind(todoItems[position])
+    override fun getItemViewType(position: Int): Int {
+        return todos[position].viewType
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (val todo = todos[position]) {
+            is Todo.Title -> (holder as TodoTitleViewHolder).bind(todo)
+            is Todo.Item -> (holder as TodoItemViewHolder).bind(todo)
+        }
     }
 
     override fun getItemCount(): Int {
-        return todoItems.size
+        return todos.size
     }
 
-    fun refresh(todoItems: List<TodoItem>) {
-        this.todoItems = todoItems
+    fun refresh(todos: List<Todo>) {
+        this.todos = todos
     }
 }
 
-data class TodoItem (
-    val text:String,
-    val checked:Boolean
-)
+sealed class Todo(val viewType: Int) {
+    data class Title(val text: String): Todo(VIEW_TYPE_TITLE)
+    data class Item(val text:String,
+                    val checked:Boolean): Todo(VIEW_TYPE_ITEM)
+
+    companion object {
+        const val VIEW_TYPE_TITLE = 0
+        const val VIEW_TYPE_ITEM = 1
+    }
+}
+
+class TodoTitleViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(R.layout.item_title, parent, false)
+) {
+    fun bind(todoTitle: Todo.Title) {
+        val textView = itemView as TextView
+        textView.text = todoTitle.text
+    }
+}
 
 class TodoItemViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
 ) {
-    fun bind(todoItem: TodoItem) {
-        val checkBox =  itemView as AppCompatCheckBox
+    fun bind(todoItem: Todo.Item) {
+        val checkBox = itemView as AppCompatCheckBox
         checkBox.text = todoItem.text
         checkBox.isChecked = todoItem.checked
     }
